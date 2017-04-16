f97d62686463219b6b8019717f56323e69b46c80
Greg Kops <greg@thinktopography.com>
2017-04-16 00:13:02 -0400

    add 1000 kittens to the database

--- examples/kittens/src/db/kittens.js
+++ examples/kittens/src/db/kittens.js
@@ -1,28 +1,22 @@
+const _ = require('lodash')
 const moment = require('moment')
+const faker = require('faker')
+
+const count = Array.apply(null, { length: 1000 }).map(Number.call, Number)
+const kittens = count.map((i, index) => ({
+  id: index + 1,
+  name: faker.fake('{{name.firstName}}'),
+  age: 5,
+  created_at: moment().subtract(2, 'months').format('YYYY-MM-DD HH:MM:ss ZZ'),
+  updated_at: moment().subtract(2, 'months').format('YYYY-MM-DD HH:MM:ss ZZ')
+}))
 
 exports.seed = function(knex, Promise) {
   return knex('kittens').del()
     .then(function () {
-      return knex('kittens').insert([
-        {
-          id: 1,
-          name: 'paws',
-          age: 5,
-          created_at: moment().subtract(2, 'months').format('YYYY-MM-DD HH:MM:ss ZZ'),
-          updated_at: moment().subtract(2, 'months').format('YYYY-MM-DD HH:MM:ss ZZ')
-        }, {
-          id: 2,
-          name: 'mittens',
-          age: 3,
-          created_at: moment().subtract(1, 'months').format('YYYY-MM-DD HH:MM:ss ZZ'),
-          updated_at: moment().subtract(1, 'months').format('YYYY-MM-DD HH:MM:ss ZZ')
-        }, {
-          id: 3,
-          name: 'socks',
-          age: 7,
-          created_at: moment().subtract(1, 'week').format('YYYY-MM-DD HH:MM:ss ZZ'),
-          updated_at: moment().subtract(1, 'week').format('YYYY-MM-DD HH:MM:ss ZZ')
-        }
-      ]);
-    });
-};
+      const chunks = _.chunk(kittens, 100)
+      return Promise.map(chunks, chunk => {
+        return knex('kittens').insert(chunk)
+      })
+    })
+}
