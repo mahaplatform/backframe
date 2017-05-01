e40e9c4761217473011e9325bb035fd0a5f5aaab
Greg Kops <greg@thinktopography.com>
2017-05-01 14:17:43 -0400

    fixing transactions

--- examples/kittens/src/app/routes.js
+++ examples/kittens/src/app/routes.js
@@ -6,7 +6,6 @@ import _ from 'lodash'
 export default resources({
   allowedParams: ['name','age'],
   model: Kitten,
-  name: 'kitten',
   filterParams: ['name','created_at'],
   serializer: KittenSerializer,
   sortParams: ['name','age']
