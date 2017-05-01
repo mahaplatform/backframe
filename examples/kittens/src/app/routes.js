93e9135b3c349faccd3118815297075c4a042e96
Greg Kops <greg@thinktopography.com>
2017-05-01 11:26:21 -0400

    implementing BackframeError and requiring bookshelf

--- examples/kittens/src/app/routes.js
+++ examples/kittens/src/app/routes.js
@@ -3,13 +3,8 @@ import Kitten from 'app/models/kitten'
 import KittenSerializer from 'app/serializers/kitten_serializer'
 import _ from 'lodash'
 
-const beforeHooks = options => (req, resolve, reject) => {
-  resolve('foo')
-}
-
 export default resources({
   allowedParams: ['name','age'],
-  beforeHooks,
   model: Kitten,
   name: 'kitten',
   filterParams: ['name','created_at'],
--- examples/kittens/src/app/services/backframe.js
+++ examples/kittens/src/app/services/backframe.js
@@ -1,7 +1,9 @@
 import backframe from 'backframe'
 import knex from './knex'
+import bookshelf from './bookshelf'
 
 export const { handler, route, resources, router, segment, table } = backframe({
+    bookshelf,
     knex,
     redis: {},
     plugins: []
