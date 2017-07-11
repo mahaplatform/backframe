e6aa89a431b67265deea21a1bb5acb428c6feddb
Greg Kops <greg@thinktopography.com>
2017-07-10 23:18:40 -0400

    rolling ou new hooks to resources

--- src/components/resources/index.js
+++ src/components/resources/index.js
@@ -18,11 +18,13 @@ export default (backframeOptions = {}) => {
   return (userOptions = {}) => {
 
     const TYPES = mergeTypes({
-      after: { type: ['function','function{}'], required: false },
+      afterCommit: { type: ['function','function{}'], required: false },
+      afterProcessor: { type: ['function','function{}'], required: false },
       allowedParams: { type: ['string[]','string[]{}'], required: false },
       alterRequest: { type: ['function','function{}'], required: false },
       alterRecord: { type: ['function','function{}'], required: false },
-      before: { type: ['function','function{}'], required: false },
+      beforeProcessor: { type: ['function','function{}'], required: false },
+      beforeRollback: { type: ['function','function{}'], required: false },
       collectionActions: { type: 'object', required: false },
       defaultParams: { type: 'function', required: false },
       defaultSort: { type: ['string','string[]'], required: false, default: '-created_at' },
